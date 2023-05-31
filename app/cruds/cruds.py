5fea998e8002be9fdd961c1d7c4cf1b4e2be9341
Sebyeol23 <lixxil2323@gmail.com>
2023-06-01 05:18:14 +0900

    userid

--- app/cruds/cruds.py
+++ app/cruds/cruds.py
@@ -26,7 +26,7 @@ async def signin(user: OAuth2PasswordRequestForm, db: Session):
         return {
             "access_token": access_token,
             "token_type": "Bearer",
-            "userid": user_exist.uid,
+            "userid": user_exist.id,
             "username": user_exist.name
         }
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid details passed")
