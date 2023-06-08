446050a70985d7112153ce86fc2faa13e7ffa557
Sebyeol23 <lixxil2323@gmail.com>
2023-06-09 03:13:19 +0900

    upcoming

--- app/cruds/cruds.py
+++ app/cruds/cruds.py
@@ -764,6 +764,7 @@ async def get_upcoming(gid: int, user: str, db: Session):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group doesn't exist")
     
     now = datetime.now()
+    return now
     current = db.query(Meeting).filter(Meeting.sdatetime <= now, Meeting.edatetime > now).first()
     if current:
         return current
